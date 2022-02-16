b7cea9f15376c393fc2828905b117ed09bf052c9
sgurnik00@gmail.com <sgurnik00@gmail.com>
2022-02-16 16:06:46 +0200

    Добавьте файлы проекта.

---
+++ PATTERNS/PATTERNS.cpp
@@ -0,0 +1,23 @@
+﻿
+#include "Prototype.h"
+int main()
+{
+    ObjectFactory::initialize();
+    Prototype* object;
+
+    /* All the object were created by cloning the prototypes. */
+    object = ObjectFactory::getType1Value1();
+    cout << object->getType() << ": " << object->getValue() << endl;
+
+    object = ObjectFactory::getType1Value2();
+    cout << object->getType() << ": " << object->getValue() << endl;
+
+    object = ObjectFactory::getType2Value1();
+    cout << object->getType() << ": " << object->getValue() << endl;
+
+    object = ObjectFactory::getType2Value2();
+    cout << object->getType() << ": " << object->getValue() << endl;
+
+
+    return 0;
+}
